320ec8c6ef6b01a5e1cc8cb236cbe0662e392685
mangbaam <pmb0836@gmail.com>
2022-10-02 17:12:04 +0900

    refactor: remove unnecessary code

--- recyclerviewclickbetter/app/src/main/java/mangbaam/practice/recyclerviewclickbetter/OrderAdapter.kt
+++ recyclerviewclickbetter/app/src/main/java/mangbaam/practice/recyclerviewclickbetter/OrderAdapter.kt
@@ -31,24 +31,13 @@ class OrderAdapter(val orderMenu: (Order) -> Unit) :
             view.cbRamen.isChecked = order.ramen
             view.cbDduckbokki.isChecked = order.dduckbokki
             view.cbCutlet.isChecked = order.cutlet
-
-            view.btnOrder.setOnClickListener {
-                val newOrder = Order(
-                    order.tableNumber,
-                    view.cbGimbob.isChecked,
-                    view.cbRamen.isChecked,
-                    view.cbDduckbokki.isChecked,
-                    view.cbCutlet.isChecked
-                )
-                orderMenu(newOrder)
-            }
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OrderViewHolder {
         val view = ItemOrderBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return OrderViewHolder(view) {
-            val order = items[it]
+        return OrderViewHolder(view) { index ->
+            val order = items[index]
             val newOrder = Order(
                 order.tableNumber,
                 view.cbGimbob.isChecked,
